beb9b6fcadcd23114a1d0a22c1833ad79280e9ed
Newtothiswholething98 <emmanueljohnso1998@gmail.com>
2025-03-27 10:25:35 -0400

    Create python.py

---
+++ python.py
@@ -0,0 +1,8 @@
+name="Emmanuel Johnson"
+age=26
+goal="software engineer"
+skills='how to write list', 'how to debug' , 'quality check software'
+print(f"My name is {name}.")
+print(f"I want to be a {goal}.")
+for skills in skills:
+print(f"I want to master {skills}.")
