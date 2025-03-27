500b28e14ebf2873b86f9f617a89212a4ee3c6ec
Newtothiswholething98 <emmanueljohnso1998@gmail.com>
2025-03-27 11:40:12 -0400

    Create python3-4.py

---
+++ python3-4.py
@@ -0,0 +1,7 @@
+name="Emmanuel"
+age=26
+topics=["automation" , "conditional scripts" , "love"]
+print(f"Hi my name is {name}, and I am {age} years old.")
+for topics in topics:
+  print(f"I want to study {topics}.")
+print(f"I want to learn a total of 3 topics") 
